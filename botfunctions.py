957ba95fa6dd09657dc283cfb42b5074afa6b6cc
Ethan-Rei <ethanrei@my.yorku.ca>
2023-01-28 14:25:59 -0500

    editing the query function

--- botfunctions.py
+++ botfunctions.py
@@ -1,11 +1,15 @@
 import discord
 import pandas
 
-sheet = pandas.read_csv('2023EECS_TIMES.csv', usecols=["Day"])
+sheet = pandas.read_csv('2023EECS_TIMES.csv')
 
 
 # TODO Output user's query (based on user_message)
 async def query_func(message, user_message):
+    # TODO add query command to find if classes are open in a building
+    # format: ?<building code> <time>
+    # format of time in military (no colon)
+    # Example ?VH 2100
     await message.channel.send(sheet)
 
 # TODO Output proper help information (depending on what user_message is)
@@ -14,3 +18,5 @@ async def help_func(message, user_message):
     # TODO list building codes
     # TODO list query commands
     await message.channel.send("hi")
+
+print(sheet["1900"])
