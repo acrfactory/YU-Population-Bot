ffd9d8326a6f16ffb7371613006b750d0a64aabd
russ-Liu <russell.liu2004@gmail.com>
2023-01-28 12:25:02 -0500

    Code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+print("hi")
 import discord
+print("hi")
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
@@ -17,6 +19,6 @@ async def on_message(message):
     user_message = str(message.content)
     channel = str(message.channel)
     if user_message.startswith("!"):
-        await message.channel.send("hi")
+        await message.channel.send("h232223142i")
 
 client.run(TOKEN)
