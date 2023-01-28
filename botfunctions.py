668dfc0a2bfd447cc0f365218fa8d27a46b33718
Ethan-Rei <ethanrei@my.yorku.ca>
2023-01-28 12:38:30 -0500

    Added a py file for user cmds

---
+++ botfunctions.py
@@ -0,0 +1,8 @@
+import discord
+# TODO Output user's query (based on user_message)
+async def query_func(message, user_message):
+    message.channel.send("hi")
+
+# TODO Output proper help information (depending on what user_message is)
+async def help_func(message, user_message):
+    message.channel.send("hi")
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import discord
+import botfunctions
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-TOKEN = ""
+TOKEN = "MTA2ODkxOTg2ODQyMzkzODA1OA.GRq0ET.xzmcJ32pPdzNAsEBTduHDNXKuLWDIK6M7exJPQ"
 
 @client.event
 async def on_ready():
     print(f"{client.user} is now running")
 
+
+# 
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -16,7 +20,15 @@ async def on_message(message):
     username = str(message.author)
     user_message = str(message.content)
     channel = str(message.channel)
+
+    # Help command
     if user_message.startswith("!"):
-        await message.channel.send("hi")
+        # Make the appropriate help 
+        await botfunctions.help_func(message, user_message[1:])
+
+    # Query command
+    if user_message.startswith("?"):
+        await botfunctions.query_func(message, user_message[1:])
+
 
 client.run(TOKEN)
