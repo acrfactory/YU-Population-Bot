7cf75d7a25030a3e6d1185c0e30797a50286b9a7
russ-Liu <russell.liu2004@gmail.com>
2023-01-28 16:40:43 -0500

    count today, anytime

--- botfunctions.py
+++ botfunctions.py
@@ -1,41 +1,60 @@
 import discord
 import pandas
-
-# from dateTime import dateTime
+from datetime import datetime
 
 sheet = pandas.read_csv('2023EECS_TIMES.csv')
 
-testData1 = sheet.iloc[:,0:1].values
-testData2 = sheet.iloc[:,2:3].values
-testData3 = sheet.iloc[:,3:4].values
+dt = datetime.now()
+date = dt.strftime('%A')[0]
+if date == "T":
+    if dt.strftime("%A")[1] == "u":
+        date = "R"
 
-w = sheet.loc[(sheet["Location"] == "WSC") & (sheet["1800"] == 1.0) ]
+testData1 = sheet.iloc[:, 0:1].values
+testData2 = sheet.iloc[:, 2:3].values
+testData3 = sheet.iloc[:, 3:4].values
+
+w = sheet.loc[(sheet["Location"] == "WSC") & (sheet["1800"] == 1.0)]
 
 
 # TODO Output user's query (based on user_message)
 async def query_func(message, user_message):
-    messageList=user_message.split(" ")
+
+    messageList = user_message.split(" ")
     building = messageList[1]
     time = messageList[2]
-    # weekday = messageList[3]
 
-    # if messageList[0] == "countClasses":
+    if messageList[0] == ("countClassesToday"): #if the command asks for the number of classes today (default)
+        # if messageList[0] == "countClasses":
         # message.channel.send(f"Building:{building}\nTime:{time}\nWeekday:{weekday}")
-    w = sheet.loc[(sheet["Location"] == f"{building}") & (sheet[f"{time}"] == 1.0) ]
-    numPeople = len(w)
+        w = sheet.loc[(sheet["Location"] == f"{building}") & (
+            sheet[f"{time}"] == 1.0) & (sheet["Day"] == f"{date}")]
+        numPeople = len(w)
 
-    if(numPeople == 1 ):
-        printMessage = f"There is {numPeople} class leaving {building} at {time}. Proceed with caution!"
-    elif (numPeople == 0 ):
-        printMessage = f"There are {numPeople} classes leaving {building} at {time}. Be extra careful!"
-    elif (numPeople >=2 ):
-        printMessage = f"There are {numPeople} classes leaving {building} at {time}. This building should be safe to walk near."
-    await message.channel.send(printMessage)
-        
+        if (numPeople == 1):
+            printMessage = f"There is {numPeople} class leaving {building} at {time} on {date}. Safer to go around, but proceed with caution"
+        elif (numPeople == 0):
+            printMessage = f"There are {numPeople} classes leaving {building} at {time} on {date}. Maybe go around another building"
+        elif (numPeople >= 2):
+            printMessage = f"There are {numPeople} classes leaving {building} at {time} on {date}. Safer to go around, but proceed with caution"
+        await message.channel.send(printMessage)
+
+    elif messageList[0] == ("countClasses"): #if the command asks for the number of classes on a certain day
+        weekday = messageList[3]
 
 
+        w = sheet.loc[(sheet["Location"] == f"{building}") & (
+            sheet[f"{time}"] == 1.0) & (sheet["Day"] == f"{weekday}")]
+        numPeople = len(w)
 
-        
+        if (numPeople == 1):
+            printMessage = f"There is {numPeople} class leaving {building} at {time} on {weekday}. Safer to go around, but proceed with caution"
+        elif (numPeople == 0):
+            printMessage = f"There are {numPeople} classes leaving {building} at {time} on {weekday}. Maybe go around another building"
+        elif (numPeople >= 2):
+            printMessage = f"There are {numPeople} classes leaving {building} at {time} on {weekday}. Safer to go around, but proceed with caution"
+        await message.channel.send(printMessage)
+
 
 # TODO Output proper help information (depending on what user_message is)
 async def help_func(message, user_message):
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-TOKEN = "MTA2ODkxOTg2ODQyMzkzODA1OA.G-MkAd.859SBlQBodORLCMIeVWXoxnEQhlt6RBLUH7EO8"
+TOKEN = "MTA2ODkxOTg2ODQyMzkzODA1OA.GMPuqk.bN_HWQx1wX4J_eR7yj1qw3HG9AmXtfun679hq0"
 
 @client.event
 async def on_ready():
@@ -32,6 +32,8 @@ async def on_message(message):
         # Returns queried data
         await botfunctions.query_func(message, user_message[1:])
 
-    if user_message == "date":
-        await botfunctions.number(message, user_message)
+    # if user_message == "date":
+    #     await botfunctions.number(message, user_message)
+
+    
 client.run(TOKEN)
