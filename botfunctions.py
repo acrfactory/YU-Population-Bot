2dea8d686e95fb84780d2f3e946e715d9a903170
Chan Woo Hwang <91679740+acrfactory@users.noreply.github.com>
2023-01-28 14:56:21 -0500

    Initial of a weekday

--- botfunctions.py
+++ botfunctions.py
@@ -1,8 +1,9 @@
 import discord
 import pandas
-
+from datetime import datetime
 sheet = pandas.read_csv('2023EECS_TIMES.csv')
-
+dt = datetime.now()
+date = dt.strftime('%A')[0]
 
 # TODO Output user's query (based on user_message)
 async def query_func(message, user_message):
@@ -18,5 +19,7 @@ async def help_func(message, user_message):
     # TODO list building codes
     # TODO list query commands
     await message.channel.send("hi")
+async def number (message, user_message):
+    await message.author.send(date)
 
 print(sheet["1900"])
