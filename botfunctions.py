02288dac993e6ca51737d37a9103c0d4b76c270f
Chan Woo Hwang <91679740+acrfactory@users.noreply.github.com>
2023-01-28 17:47:53 -0500

    Help

--- botfunctions.py
+++ botfunctions.py
@@ -22,7 +22,7 @@ BUILDING_CODES = {
     "LAS": "Lassonde Building",
     "SC": "Stong College"
 }
-
+TIMES = set(["1800", "1830", "1900", "1930", "2000", "2030", "2100", "2130"])
 async def query_func(message, user_message):
 
     fields = user_message.split(" ")
@@ -62,5 +62,25 @@ async def query_func(message, user_message):
             printMessage = f"There are {numPeople} classes leaving {building} at {time} on {wday}. Safer to go around, but proceed with caution"
         await message.channel.send(printMessage)
 # TODO Output proper help information (depending on what user_message is)
-async def help_func(message, user_message):
-    await message.channel.send(COMMANDS)
+ # ?contains <building code> <time>
+    elif fields[0] == "contains":
+        if len(fields) == 1 or fields[1] not in BUILDING_CODES:
+            await message.channel.send("Improper building code")
+            return
+        if len(fields) == 2 or fields[2] not in TIMES:
+            await message.channel.send("Improper time")
+            return
+        # TODO check
+        if len(sheet.loc[ (sheet[fields[2]] == 1.0) & (sheet["Location"] == fields[1]) ]) != 0:
+            await message.channel.send(f"{BUILDING_CODES[fields[1]]} contains classes during {fields[2][:2]}:{fields[2][2:]}")
+        else:
+            await message.channel.send(f"{BUILDING_CODES[fields[1]]} does not contain classes during {fields[2][:2]}:{fields[2][2:]}")
+
+    # ?listCodes
+    elif fields[0] == "listCodes":
+        for code, building in COMMANDS.items():
+            await message.channel.send(f"{code}: {building}")
+# ?help
+    elif fields[0] == "help":
+        for command, desc in COMMANDS.items():
+            await message.channel.send(f"{command}: {desc}")
