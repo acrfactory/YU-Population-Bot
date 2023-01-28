0a7233376a81ab2bc166fb1b940e6c8305485a66
Ethan-Rei <ethanrei@my.yorku.ca>
2023-01-28 12:01:38 -0500

    things from video

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-print("hi")
-print("pull test")
-print("pull test2")
-print ("JN test")
+TOKEN = "MTA2ODkxOTg2ODQyMzkzODA1OA.Gx_qoK.zxNBIFAr5M8MyfcRAPGkPVxkNQVxNqJ5ZfGfoc"
+
+@client.event
+async def on_ready():
+    print(f"{client.user} is now running")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel)
+    if user_message.startswith("!"):
+        message.channel.send("hi")
+
+client.run()
