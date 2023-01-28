fb87bf3ce6156a1d504f369a1f752746658adc3b
Chan Woo Hwang <91679740+acrfactory@users.noreply.github.com>
2023-01-28 17:04:15 -0500

    First Commit

--- botfunctions.py
+++ botfunctions.py
@@ -1,26 +1,37 @@
 import discord
 import pandas
+from datetime import datetime
 
 sheet = pandas.read_csv('2023EECS_TIMES.csv')
-from datetime import datetime
-dt = datetime.now()
-date = dt.strftime('%A')[0]
 
+dt = datetime.now()
+weekday = dt.strftime('%A')[0]
+if weekday == "T":
+    if dt.strftime("%A")[1] == "u":
+        weekday = "R"
+    if dt.strftime("%A")[0] == "S":
+        weekday = "M"
 # TODO Output user's query (based on user_message)
 async def query_func(message, user_message):
-    # TODO add query command to find if classes are open in a building
-    # format: ?<building code> <time>
-    # format of time in military (no colon)
-    # Example ?VH 2100
-    await message.channel.send(sheet)
+
+    messageList = user_message.split(" ")
+    building = messageList[1]
+    time = "2030"
+    if messageList[0] == ("countClasses"): #if the command asks for the number of classes on a certain day
+
+        w = sheet.loc[(sheet["Location"] == f"{building}") & (
+            sheet[f"{time}"] == 1.0) & (sheet["Day"] == f"{weekday}")]
+        numPeople = len(w)
+
+        if (numPeople == 1):
+            printMessage = f"There is {numPeople} class leaving {building} at {time} on {weekday}. Safer to go around, but proceed with caution"
+        elif (numPeople == 0):
+            printMessage = f"There are {numPeople} classes leaving {building} at {time} on {weekday}. Maybe go around another building"
+        elif (numPeople >= 2):
+            printMessage = f"There are {numPeople} classes leaving {building} at {time} on {weekday}. Safer to go around, but proceed with caution"
+        await message.channel.send(printMessage)
+
 
 # TODO Output proper help information (depending on what user_message is)
 async def help_func(message, user_message):
-    # TODO list commands
-    # TODO list building codes
-    # TODO list query commands
     await message.channel.send("hi")
-async def number (message, user_message):
-    await message.author.send(date)
-
-print(sheet["1900"])
