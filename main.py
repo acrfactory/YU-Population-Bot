a327051c223b2005c11d79a2b15563417d4e0d46
Chan Woo Hwang <91679740+acrfactory@users.noreply.github.com>
2023-01-28 16:49:10 -0500

    Commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-TOKEN = "MTA2ODkxOTg2ODQyMzkzODA1OA.GU7tRN.-fd79inUt1_F8L7IpyKGZx4gu4XHpJAQkZUKFw"
+TOKEN = "MTA2ODkxOTg2ODQyMzkzODA1OA.GI6cfC.8-FJjksqrhy9sQXS5ifWj1FFU7JXl208DV9FcM"
 
 @client.event
 async def on_ready():
@@ -31,6 +31,6 @@ async def on_message(message):
         # Returns queried data
         await botfunctions.query_func(message, user_message[1:])
 
-    if user_message == "date":
-        await botfunctions.number(message, user_message)
+    if user_message.startswith(";"):
+        await message.author.send(date[3])
 client.run(TOKEN)
