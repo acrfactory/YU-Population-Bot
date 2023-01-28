6d2fb2b94c8573b756d8799c6645fad86440565a
Chan Woo Hwang <91679740+acrfactory@users.noreply.github.com>
2023-01-28 14:13:42 -0500

    dotfunctions pandas

--- botfunctions.py
+++ botfunctions.py
@@ -1,11 +1,13 @@
 import discord
 import pandas
+
+sheet = pandas.read_csv('2023EECS_TIMES.csv', usecols=["Day"])
+
+
 # TODO Output user's query (based on user_message)
 async def query_func(message, user_message):
-    await message.channel.send("hi")
+    await message.channel.send(sheet)
 
 # TODO Output proper help information (depending on what user_message is)
 async def help_func(message, user_message):
     await message.channel.send("hi")
-
-
