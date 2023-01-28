c60e3aef2930bb8d63ebc15b64272ea476834c07
Chan Woo Hwang <91679740+acrfactory@users.noreply.github.com>
2023-01-28 14:02:56 -0500

    Date attribute added

--- botfunctions.py
+++ botfunctions.py
@@ -7,3 +7,5 @@ async def query_func(message, user_message):
 # TODO Output proper help information (depending on what user_message is)
 async def help_func(message, user_message):
     await message.channel.send("hi")
+
+
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-TOKEN = "MTA2ODkxOTg2ODQyMzkzODA1OA.GU7CNu.4mwY7QF8Ga5CNVYSu0pRsKgGdfRyTn8xhpjyfc"
+TOKEN = "MTA2ODkxOTg2ODQyMzkzODA1OA.GY-VbW.CMm9JJ6yILz4-OxXx6ust56KM6pBs2I2cwWycI"
 
 @client.event
 async def on_ready():
@@ -20,7 +20,7 @@ async def on_message(message):
     username = str(message.author)
     user_message = str(message.content)
     channel = str(message.channel)
-
+    date = str(message.created_at)#Date the message sent
     # Help command
     if user_message.startswith("!"):
         # Make the appropriate help 
@@ -30,5 +30,4 @@ async def on_message(message):
     if user_message.startswith("?"):
         await botfunctions.query_func(message, user_message[1:])
 
-
-client.run()
+client.run(TOKEN)
