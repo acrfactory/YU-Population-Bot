9b7f38a08d06d8a6480288b34a574b0291a94ab1
JamilleN <jamilletni@gmail.com>
2023-01-28 11:49:32 -0500

    remove testing msgs

--- main.py
+++ main.py
@@ -2,8 +2,3 @@ import discord
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-
-print("hi")
-print("pull test")
-print("pull test2")
-print ("JN test")
