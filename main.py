0fd16f7e19da9653876e09737b047672cae52bf6
rusliu <120234117+rusliu@users.noreply.github.com>
2023-01-28 19:09:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-print("hi")
 import discord
 import botfunctions
 import os
@@ -7,7 +6,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-TOKEN = "MTA2ODkxOTg2ODQyMzkzODA1OA.GMPuqk.bN_HWQx1wX4J_eR7yj1qw3HG9AmXtfun679hq0"
+TOKEN = ""
 
 @client.event
 async def on_ready():
@@ -22,18 +21,10 @@ async def on_message(message):
     user_message = str(message.content)
     channel = str(message.channel)
 
-    # Help command
-    if user_message.startswith("!"):
-        # Sends appropriate help message
-        await botfunctions.help_func(message, user_message[1:])
-
     # Query command
     if user_message.startswith("?"):
         # Returns queried data
         await botfunctions.query_func(message, user_message[1:])
-
-    # if user_message == "date":
-    #     await botfunctions.number(message, user_message)
 
     
 client.run(TOKEN)
